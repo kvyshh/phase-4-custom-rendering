3162ba7075de66cb057167fd812d6cdce6434b57
Kindred Lee <kindred4lee@gmail.com>
2021-12-20 06:52:07 -0800

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,7 +3,11 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    if cheese
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+    else 
+      render json: { error: "Cheese not found" }, status: :not_found
+    end
   end
 
   # GET /cheeses/:id
